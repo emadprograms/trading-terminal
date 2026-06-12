078ac7e0020228842b8d60012dba93ffc5859dca
Hamza Arshad Alam <79793120+emadprograms@users.noreply.github.com>
2026-06-12 10:07:24 +0300

    style: lighten HC gridlines

--- src/hooks/useChartLifecycle.ts
+++ src/hooks/useChartLifecycle.ts
@@ -158,7 +158,7 @@ export function useChartLifecycle({
     if (highContrast) {
         initChartRef.current.applyOptions({
             layout: { background: { color: '#cccccc' }, textColor: '#000000' },
-            grid: { vertLines: { color: 'rgba(0, 0, 0, 0.12)' }, horzLines: { color: 'rgba(0, 0, 0, 0.12)' } },
+            grid: { vertLines: { color: 'rgba(0, 0, 0, 0.06)' }, horzLines: { color: 'rgba(0, 0, 0, 0.06)' } },
             timeScale: { borderColor: '#a3a3a3' }
         });
         initChartRef.current.priceScale('right').applyOptions({
