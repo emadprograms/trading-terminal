6beb74e0a3ea4f822c1a417f597ead8b4bffc29c
Hamza Arshad Alam <79793120+emadprograms@users.noreply.github.com>
2026-08-28 10:15:49 +0300

    chore: remove scratch files

--- scratch_patch.ts
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-const file = 'src/components/AccountHeader.tsx';
-let content = fs.readFileSync(file, 'utf8');
-content = content.replace(
-  "const accounts = await accountApi.fetchAccounts();",
-  "let accounts: any; try { accounts = await accountApi.fetchAccounts(); } catch (err) { console.error('[AccountHeader] fetch error:', err); throw err; }"
-);
-fs.writeFileSync(file, content);
